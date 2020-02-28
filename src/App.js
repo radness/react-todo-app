18ea01a1abde913735b6878626ae4e51558e1aff
radness <yhs5128@gmail.com>
2020-02-28 23:29:58 +0900

    11.1 많은 데이터 렌더링 하기

--- src/App.js
+++ src/App.js
@@ -3,24 +3,20 @@ import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
       id: 1,
-      text: '리액트 기초 알아보기',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: '컴포넌트 스타일링 해보기',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: '일정관리 앱 만들어 보기',
+      text: `Todo ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
+
+const App = () => {
+  const [todos, setTodos] = useState(createBulkTodos);
 
   // 고유값으로 사용될 id
   // ref를 사용하여 변수를 담는다.
